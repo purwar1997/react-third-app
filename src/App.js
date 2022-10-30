0fbd5caac78a387f335ca90e549c42d302b2b947
purwar1997 <shubhampurwar511@gmail.com>
2022-10-30 22:14:29 +0530

    more components added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 // function name can be anything but has to start with a capital letter
+// all the components are bundled inside App() function
 function App() {
   return (
     <>
